6097d07a980d24bfa13a870dde11102f6fe77295
Denys Pavlenko <de.pavlenko@gmail.com>
2025-12-23 12:59:26 +0200

    feat(app.config): bump version to 1.0.1

--- app.config.ts
+++ app.config.ts
@@ -29,7 +29,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name: getAppName(),
     slug: "mindstoria",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./src/assets/images/icon.png",
     scheme: "mindstoria",
